40ed25cfd99dae138af5ec25aa341bb1025377ee
gllez <42877409+gllez@users.noreply.github.com>
2018-11-19 19:53:05 -0800

    Grafitos

---
+++ Grafos/Grafos/Proceso.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Proceso
+    {
+        public void Grafo()
+        {
+            Console.WriteLine("a -> b -> c -> d -> e -> f-> g");
+            Console.WriteLine("a -> b -> c -> g -> f -> d-> e");
+            Console.WriteLine("a -> b -> c -> g -> f -> e-> d");    // Aqui se muestran todos los caminos hechos de manera manual y de la unica manera que se por el momento
+            Console.WriteLine("a -> b -> g -> c -> d -> f-> e");
+            Console.WriteLine("a -> b -> g -> c -> d -> e-> f");
+            Console.WriteLine("a -> b -> g -> f -> e -> d-> c");
+        }
+    }
+}
---
+++ Grafos/Grafos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceso proceso = new Proceso();
+
+            Console.WriteLine("Caminos existentes: \n");
+            proceso.Grafo();
+            Console.ReadKey();
+        }
+    }
+}
